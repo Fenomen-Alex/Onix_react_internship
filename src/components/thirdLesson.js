c841f2b75220cf32a8ae9caee810f5c06a5441eb
Fenomen-Alex <alexfenomen@gmail.com>
2021-02-15 17:21:47 +0200

    adding ReactStrap

--- src/components/thirdLesson.js
+++ src/components/thirdLesson.js
@@ -7,7 +7,6 @@ import { Button } from 'reactstrap';
 const LessonThree = () => {
     return(
     <div>
-        <p id="myLog"></p>
         <Button color="primary">primary</Button>
         <Button color="primary">primary</Button>
     </div>
